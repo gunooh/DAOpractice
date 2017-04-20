23598f8fd849031e40329766806acc2920d23d02
gunooh <gunooh12@gmail.com>
2017-04-20 19:47:34 +0900

    01

---
+++ src/kr/ac/jejunu/UserDao.java
@@ -0,0 +1,48 @@
+package kr.ac.jejunu;
+
+import java.sql.*;  // SQL문을 사용하기 위해 필요
+
+/**
+ * Created by hyh0408 on 2017. 3. 15..
+ */
+public  class UserDao {
+    public User get(String id) throws ClassNotFoundException, SQLException {
+
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/portal", "root", "4693");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id = ?");
+        preparedStatement.setString(1, id);
+        ResultSet resultSet =  preparedStatement.executeQuery();
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getString("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return user;
+    }
+    // 파라미터안에 User 클래스의 user 객체가 들어가는 이유 : User 클래스의 getId 메소드를 받아오기 위해
+
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/portal", "root", "4693");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into user(id, name, password) values(?, ?, ?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getName());
+        preparedStatement.setString(3, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement.close();
+        connection.close();
+    }
+
+}
---
+++ test/kr/ac/jejunu/UserDaoTest.java
@@ -0,0 +1,52 @@
+package kr.ac.jejunu;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by PARK on 2017-04-15.
+ */
+public class UserDaoTest {
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+        String id = "1";
+        String name = "박건우";
+        String password = "1234";
+        User user = userDao.get(id);
+        assertThat(id, is(user.getId()));
+        assertThat(name, is(user.getName()));
+        assertThat(password, is(user.getPassword()));
+    }
+
+
+
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        String id = String.valueOf(new Random().nextInt());
+        String name = "박건우";
+        String password = "1234";
+
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.add(user);
+
+        User addedUser = userDao.get(id);
+
+        assertThat(id, is(addedUser.getId()));
+        assertThat(name, is(addedUser.getName()));
+        assertThat(password, is(addedUser.getPassword()));
+
+    }
+
+}
