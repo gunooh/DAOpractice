e0df2882d57d6b3f9a31b5bbfd3c35a57d676dd9
gunooh <gunooh12@gmail.com>
2017-04-21 03:08:06 +0900

    12

--- src/kr/ac/jejunu/UserDao.java
+++ src/kr/ac/jejunu/UserDao.java
@@ -16,22 +16,45 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
-
-        return jdbcContext.jdbcContextWithStatementStrategyForQuery(id, statementStrategy);
+        return jdbcContext.jdbcContextWithStatementStrategyForQuery(id, new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("select * from user where id = ?");
+                preparedStatement.setString(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
     // 파라미터안에 User 클래스의 user 객체가 들어가는 이유 : User 클래스의 getId 메소드를 받아오기 위해
-    public void add(User user) throws SQLException, ClassNotFoundException {
-        StatementStrategy statementStrategy = new AddUserStatementStrategy(user);
+    public void add(final User user) throws SQLException, ClassNotFoundException {
 
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("insert into user(id, name, password) values(?, ?, ?)");
+
+                preparedStatement.setString(1, user.getId());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getPassword());
+                return  preparedStatement;
+            }
+        });
     }
 
     public void delete(String id) throws SQLException {
-        StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
 
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy(){
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("delete from user where id = ?");
+                preparedStatement.setString(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
     public void setJdbcContext(JdbcContext jdbcContext) {
